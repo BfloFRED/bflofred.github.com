b1a51fb4c465387ef22a22b052f32520fffe1758
Gray Ghost Visuals <grayghost@grayghostvisuals.com>
2012-12-11 20:57:00 -0500

    Grunt Ready!

---
+++ grunt.js
@@ -0,0 +1,60 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    meta: {
+      version: '0.1.0',
+      banner: '/*! PROJECT_NAME - v<%= meta.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '* http://PROJECT_WEBSITE/\n' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+        'YOUR_NAME; Licensed MIT */'
+    },
+    lint: {
+      files: ['dist/concat.js']
+    },
+    qunit: {
+      files: ['index.html']
+    },
+    concat: {
+      dist: {
+        src: ['js/plugins.js', 'js/main.js'],
+        dest: 'dist/concat.js'
+      }
+    },
+    min: {
+      dist: {
+        src: ['dist/concat.js'],
+        dest: 'js/minified/main-min.js'
+      }
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint qunit'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {
+        jQuery: true
+      }
+    },
+    uglify: {}
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint qunit concat min');
+
+};

161b343636ac726e6514b24108c6b4db009abb36
Gray Ghost Visuals <grayghost@grayghostvisuals.com>
2013-01-22 22:08:00 -0500

    Google Maps API Key, Markup Adjustments, DNS prefetching, Fresh Codekit Config

--- js/main.js
+++ js/main.js
@@ -1,43 +1,86 @@
-//https://maps.google.com/maps?q=225+delaware+avenue,+Buffalo,+NY&hl=en&sll=42.746632,-75.770041&sspn=5.977525,8.591309&t=h&hnear=225+Delaware+Ave,+Buffalo,+Erie,+New+York+14202&z=16
-var sw        = document.body.clientWidth,
-    bp        = 550,
-    map       = $('.map'),
-    staticmap = '//maps.googleapis.com/maps/api/staticmap?center=42.890595,-78.876607&zoom=15&markers=42.890595,-78.876607&size=640x320&sensor=true',
-    embed     = '<iframe width="980" height="650" frameborder="0" scrolling="no" class="google-map" marginheight="0" marginwidth="0" src="//maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=225+delaware+avenue,+buffalo+ny&amp;aq=&amp;sll=42.746632,-75.770041&amp;sspn=5.977525,8.580322&amp;ie=UTF8&amp;hq=&amp;hnear=225+Delaware+Ave,+Buffalo,+Erie,+New+York+14202&amp;t=m&amp;z=14&amp;iwloc=A&amp;ll=42.890595,-78.876607&amp;output=embed"></iframe>';
+// https://maps.google.com/maps?q=225+delaware+avenue,+Buffalo,+NY&hl=en&sll=42.746632,-75.770041&sspn=5.977525,8.591309&t=h&hnear=225+Delaware+Ave,+Buffalo,+Erie,+New+York+14202&z=16
+// API key console
+// https://code.google.com/apis/console
+// BfloFRED API Key : key=AIzaSyBMSezbmos0W2n6BQutkFvNjkF5NTPd0-Q
+var sw         = document.body.clientWidth,
+    bp         = 550,
+    map        = $('.map'),
+    // Google Static Maps API Docs
+    // https://developers.google.com/maps/documentation/staticmaps
+    staticmap  = '//maps.googleapis.com/maps/api/staticmap?center=42.890595,-78.876607key=AIzaSyBMSezbmos0W2n6BQutkFvNjkF5NTPd0-Q&zoom=15&size=800x400&markers=42.890595,-78.876607&size=640x320&maptype=terrain&sensor=true',
+    // Google Dynamic API Docs
+    embed      = '<iframe width="980" height="650" frameborder="0" scrolling="no" class="google-map" marginheight="0" marginwidth="0" src="//maps.google.com/maps?f=q&amp;source=s_q&amp;key=AIzaSyBMSezbmos0W2n6BQutkFvNjkF5NTPd0-Q&amp;hl=en&amp;geocode=&amp;q=225+delaware+avenue,+buffalo+ny&amp;aq=&amp;sll=42.746632,-75.770041&amp;sspn=5.977525,8.580322&amp;ie=UTF8&amp;hq=&amp;hnear=225+Delaware+Ave,+Buffalo,+Erie,+New+York+14202&amp;t=m&amp;z=14&amp;iwloc=A&amp;ll=42.890595,-78.876607&amp;output=embed"></iframe>';
 
 
-function buildEmbed() { //Build iframe view
-   $('<div class="map-container"/>').html(embed).prependTo(map);
+// $Build iframe view
+// ============================================================
+
+function googleMapAPIScript() {
+  var script     = document.createElement("script");
+      script.async;
+      // Load the Google Maps API : required
+      // https://developers.google.com/maps/documentation/javascript/tutorial
+      script.src = "http://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyBMSezbmos0W2n6BQutkFvNjkF5NTPd0-Q&sensor=true";
+  document.head.appendChild(script);
 }
 
 
-function buildStatic() { //Build static map
-   var mapLink = $('.map-link').attr('href'),
-       $img    = $('<img class="static-map-img" alt="">').attr('src', staticmap);
+// $Build iframe view
+// ============================================================
 
-       $('<a/>').attr('href', mapLink).html($img).prependTo(map);
+function buildEmbed() {
+    $('<div class="map-container"/>').html(embed).prependTo(map);
 }
 
+
+// $Build static map
+// ============================================================
+
+function buildStatic() {
+    var mapLink = $('.map-link').attr('href'),
+        $img    = $('<img class="static-map-img" alt="">').attr('src', staticmap);
+
+    $('<a/>').attr('href', mapLink).html($img).prependTo(map);
+}
+
+
+// $If Large Screen
+// ============================================================
 
 function buildMap() {
-    if (sw > bp) { //If Large Screen
-        if ($('.map-container').length < 1) { //If map doesn't already exist
+    if (sw > bp) {
+        // If map doesn't already exist
+        if ($('.map-container').length < 1) {
             buildEmbed();
         }
     } else {
-        if ($('.static-map-img').length < 1) { //If static image doesn't exist
+         // If static image doesn't exist
+        if ($('.static-map-img').length < 1) {
             buildStatic();
         }
     }
 }
 
 
+// $Window Onlod Events
+// ============================================================
+
+window.onload = googleMapAPIScript;
+
+
+// $Window Resizing
+// ============================================================
+
 $(window).resize(function () {
     sw = document.body.clientWidth;
     buildMap();
-    google.maps.event.trigger(map, "resize");
+    // this is causing an 'undefined' call for google
+    google.maps.event.trigger(map, 'resize');
 });
 
+
+// $Document Ready
+// ============================================================
 
 $(document).ready(function () {
     // Build the Google Map
